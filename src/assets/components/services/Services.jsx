d03a714b26e848daeadd0436731e4afed6de5543
reandyferdinanto <reandyferdinanto@gmail.com>
2023-03-21 14:39:05 +0700

    update my career timeline

--- src/assets/components/services/Services.jsx
+++ src/assets/components/services/Services.jsx
@@ -5,112 +5,126 @@ import { BiCheck } from "react-icons/bi";
 const Services = () => {
   return (
     <section id="services">
-      <h5>What I Offer</h5>
-      <h2>Services</h2>
+      <h5>Experience</h5>
+      <h2>My Career</h2>
 
       <div className="container service__container">
         <article className="services">
           <div className="service__head">
-            <h3>Mobile App - GlideApps</h3>
+            <h3>Warehouse Supervisor & IT Support</h3>
+            <h5>2018 - 2022 </h5>
+            <h4>PT. Expertest Kaliper Nusantara</h4>
           </div>
 
           <ul className="service__list">
+            <h4>Warehouse Supervisor</h4>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>1 Lorem, ipsum</p>
+              <p>
+                Plan, organized, supervised, and participated in daily warehouse
+                operation
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>2 Lorem, ipsum</p>
+              <p>
+                Supported production workers in meeting goals without
+                compromising safety and compliance
+              </p>
+            </li>
+            <h4>IT Support</h4>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Installing, configuring, and maintaining hardware & software
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>3 Lorem, ipsum</p>
+              <p>
+                Assisting IT Supervisor with any troubleshooting on the server
+                remotely
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>4 Lorem, ipsum</p>
+              <p>
+                Evaluate current hardware and software needs and identify
+                opportunities for improvement
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>5 Lorem, ipsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>6 Lorem, ipsum</p>
+              <p>
+                Recommend and purchase new hardware and software solutions that
+                meet the company's needs and budget
+              </p>
             </li>
           </ul>
         </article>
 
         {/* END OF WEBSITE DESIGN */}
+
         <article className="services">
           <div className="service__head">
-            <h3>Web Development</h3>
+            <h3>Switch Career Program - Backend Developer Bootcamp </h3>
+            <h5>Juni - December 2022</h5>
+            <h4>Dibimbing.id</h4>
           </div>
 
           <ul className="service__list">
             <li>
               <BiCheck className="service__list-icon" />
-              <p>1 Lorem, ipsum</p>
+              <p>Developed web platform back ends using node.JS</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>2 Lorem, ipsum</p>
+              <p>Developed server-side logic in JavaScript</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>3 Lorem, ipsum</p>
+              <p>Built APIs and data clients to consume APIs</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>4 Lorem, ipsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>5 Lorem, ipsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>6 Lorem, ipsum</p>
-            </li>
-          </ul>
-        </article>
-
-        {/* END OF WEB DEVELOPMENT */}
-        <article className="services">
-          <div className="service__head">
-            <h3>BackEnd Development </h3>
-          </div>
-
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>1 Lorem, ipsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>2 Lorem, ipsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>3 Lorem, ipsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>4 Lorem, ipsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>5 Lorem, ipsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>6 Lorem, ipsum</p>
+              <p>Tested and documented with POSTMAN application</p>
             </li>
           </ul>
         </article>
 
         {/* END OF BACKEND DEVELOPMENT */}
+        <article className="services">
+          <div className="service__head">
+            <h3>Project-based Intern: Backend Developer Vitual Internship</h3>
+            <h5>February - March 2023</h5>
+            <h4>Evermos - Rakamin </h4>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Developed inventory API using Golang and Fiber</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Gained experience in managing and securing APIs</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Worked with MySQL and SQL queries to manage inventory data</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Learned software development best practices from mentor</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Added inventory API to portfolio to showcase skills</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* END OF WEB DEVELOPMENT */}
       </div>
     </section>
   );
