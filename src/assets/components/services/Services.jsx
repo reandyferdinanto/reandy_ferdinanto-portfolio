76d452b339ef6994bb66f16c3d95d239686d03fb
reandyferdinanto <reandyferdinanto@gmail.com>
2023-03-21 14:44:58 +0700

    re-arrange the list of career

--- src/assets/components/services/Services.jsx
+++ src/assets/components/services/Services.jsx
@@ -11,6 +11,68 @@ const Services = () => {
       <div className="container service__container">
         <article className="services">
           <div className="service__head">
+            <h3>Project-based Intern: Backend Developer Vitual Internship</h3>
+            <h5>February - March 2023</h5>
+            <h4>Evermos - Rakamin </h4>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Developed inventory API using Golang and Fiber</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Gained experience in managing and securing APIs</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Worked with MySQL and SQL queries to manage inventory data</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Learned software development best practices from mentor</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Added inventory API to portfolio to showcase skills</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* END OF EVERMOS */}
+
+        <article className="services">
+          <div className="service__head">
+            <h3>Switch Career Program - Backend Developer Bootcamp </h3>
+            <h5>Juni - December 2022</h5>
+            <h4>Dibimbing.id</h4>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Developed web platform back ends using node.JS</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Developed server-side logic in JavaScript</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Built APIs and data clients to consume APIs</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Tested and documented with POSTMAN application</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* END OF DIBIMBING */}
+
+        <article className="services">
+          <div className="service__head">
             <h3>Warehouse Supervisor & IT Support</h3>
             <h5>2018 - 2022 </h5>
             <h4>PT. Expertest Kaliper Nusantara</h4>
@@ -63,68 +125,7 @@ const Services = () => {
           </ul>
         </article>
 
-        {/* END OF WEBSITE DESIGN */}
-
-        <article className="services">
-          <div className="service__head">
-            <h3>Switch Career Program - Backend Developer Bootcamp </h3>
-            <h5>Juni - December 2022</h5>
-            <h4>Dibimbing.id</h4>
-          </div>
-
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Developed web platform back ends using node.JS</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Developed server-side logic in JavaScript</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Built APIs and data clients to consume APIs</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Tested and documented with POSTMAN application</p>
-            </li>
-          </ul>
-        </article>
-
-        {/* END OF BACKEND DEVELOPMENT */}
-        <article className="services">
-          <div className="service__head">
-            <h3>Project-based Intern: Backend Developer Vitual Internship</h3>
-            <h5>February - March 2023</h5>
-            <h4>Evermos - Rakamin </h4>
-          </div>
-
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Developed inventory API using Golang and Fiber</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Gained experience in managing and securing APIs</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Worked with MySQL and SQL queries to manage inventory data</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Learned software development best practices from mentor</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Added inventory API to portfolio to showcase skills</p>
-            </li>
-          </ul>
-        </article>
-
-        {/* END OF WEB DEVELOPMENT */}
+        {/* END OF EKN */}
       </div>
     </section>
   );
